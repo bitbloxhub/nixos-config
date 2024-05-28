fe93c79e1408e3ccbb99b866eda8600939d96d35
bitbloxhub <45184892+bitbloxhub@users.noreply.github.com>
2024-05-28 17:14:27 +0000

    git ssh keys

--- home.nix
+++ home.nix
@@ -58,6 +58,15 @@
     };
   };
 
+  programs.ssh = {
+    enable = true;
+    matchBlocks = {
+      "github.com" = {
+        identityFile = "/home/jonahgam/.ssh/id_ed25519_github";
+      };
+    };
+  };
+
   programs.kitty = {
     enable = true;
     font.name = "Fira Code Light";
