1f3840249a26c8943954aada8a34dd53bab94429
bitbloxhub <45184892+bitbloxhub@users.noreply.github.com>
2024-05-28 17:34:30 +0000

    disko 1

--- flake.nix
+++ flake.nix
@@ -4,22 +4,23 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     catppuccin.url = "github:catppuccin/nix/main";
+    disko = {
+      url = "github:nix-community/disko/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     home-manager = {
       url = "github:nix-community/home-manager/master";
-      # The `follows` keyword in inputs is used for inheritance.
-      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
-      # the `inputs.nixpkgs` of the current flake,
-      # to avoid problems caused by different versions of nixpkgs.
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, catppuccin, home-manager, ... }@inputs: {
+  outputs = { self, nixpkgs, catppuccin, disko, home-manager, ... }@inputs: {
     nixosConfigurations.nixos-bill = nixpkgs.lib.nixosSystem {
       system = "x86_64_linux";
       modules = [
         catppuccin.nixosModules.catppuccin
         ./hosts/nixos-bill
+        disko.nixosModules.disko
         home-manager.nixosModules.home-manager {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
