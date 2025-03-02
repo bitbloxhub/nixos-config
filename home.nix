97d168a8fd0c26b1c7ab4fcfd9ce06dd223a0280
bitbloxhub <45184892+bitbloxhub@users.noreply.github.com>
2025-03-02 22:12:08 +0000

    feat(nvim): add AniMotion.nvim

--- home.nix
+++ home.nix
@@ -103,29 +103,41 @@
             ];
         };
         startupPlugins = {
-          general = with pkgs.vimPlugins; [
-            mini-nvim
-            catppuccin-nvim
-            fidget-nvim
-            nvim-lspconfig
-            nvim-treesitter.withAllGrammars
-            blink-cmp
-            direnv-vim
-            neo-tree-nvim
-            fzf-lua
-            render-markdown-nvim
-            image-nvim
-            snacks-nvim
-            edgy-nvim
-            flatten-nvim
-            molten-nvim
-            jupytext-nvim
-            otter-nvim
-            quarto-nvim
-            git-conflict-nvim
-            precognition-nvim
-            hardtime-nvim
-          ];
+          general =
+            let
+              animotion-nvim = pkgs.vimUtils.buildVimPlugin {
+                name = "AniMotion.nvim";
+                src = builtins.fetchGit {
+                  url = "https://github.com/luiscassih/AniMotion.nvim.git";
+                  rev = "a1adf214e276fa8c3f439ce3fa13a6f647744dab";
+                };
+              };
+            in
+            with pkgs.vimPlugins;
+            [
+              mini-nvim
+              catppuccin-nvim
+              fidget-nvim
+              nvim-lspconfig
+              nvim-treesitter.withAllGrammars
+              blink-cmp
+              direnv-vim
+              neo-tree-nvim
+              fzf-lua
+              render-markdown-nvim
+              image-nvim
+              snacks-nvim
+              edgy-nvim
+              flatten-nvim
+              molten-nvim
+              jupytext-nvim
+              otter-nvim
+              quarto-nvim
+              git-conflict-nvim
+              precognition-nvim
+              hardtime-nvim
+              animotion-nvim
+            ];
         };
       }
     );
