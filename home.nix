ef1f787209bc5fae97f17cc844950f47d2bb5a3d
bitbloxhub <45184892+bitbloxhub@users.noreply.github.com>
2025-03-03 02:25:25 +0000

    feat: hyprland and stuff

--- home.nix
+++ home.nix
@@ -25,19 +25,54 @@
     system-manager.packages."${pkgs.system}".default
   ];
 
+  services.hyprpaper.enable = true;
+  services.hyprpaper.settings = {
+    ipc = "on";
+    splash = false;
+    splash_offset = 2.0;
+
+    preload = [ "/home/jonahgam/.local/share/eog-wallpaper.png" ];
+    wallpaper = [ ",/home/jonahgam/.local/share/eog-wallpaper.png" ];
+  };
+
+  programs.waybar.enable = true;
+  programs.waybar.style = builtins.readFile ./waybar.css;
+  programs.waybar.settings = [
+    {
+      layer = "top";
+      position = "top";
+      output = [ "eDP-1" ];
+      modules-left = [ "hyprland/workspaces" ];
+      modules-center = [
+        "hyprland/window"
+      ];
+      modules-right = [
+        "pulseaudio"
+        "battery"
+        "clock"
+      ];
+    }
+  ];
+
   wayland.windowManager.hyprland.enable = true;
   wayland.windowManager.hyprland.settings = {
     "$mod" = "SUPER";
-    monitor = ",eDP-1,auto,auto";
+    monitor = ",preferred,auto,1";
     env = [
       "LIBVA_DRIVER_NAME,nvidia"
       "XDG_SESSION_TYPE,wayland"
       "GBM_BACKEND,nvidia-drm"
       "__GLX_VENDOR_LIBRARY_NAME,nvidia"
     ];
+    exec-once = [
+      "waybar"
+    ];
     bind =
       [
         "$mod, F, exec, firefox"
+        "$mod, T, exec, wezterm"
+        "$mod, M, fullscreen, 1"
+        "ALT, Tab, cyclenext"
         "CTRL ALT, Delete, exec, hyprctl dispatch exit 0"
         ", Print, exec, grimblast copy area"
       ]
@@ -57,6 +92,20 @@
           ) 9
         )
       );
+    windowrulev2 = [
+      "maximize, class:.*"
+    ];
+    misc = {
+      exit_window_retains_fullscreen = true;
+    };
+    input.touchpad.tap-to-click = false;
+    group = {
+      groupbar.enabled = true;
+    };
+    ecosystem = {
+      no_update_news = true;
+      no_donation_nag = true;
+    };
     debug.disable_logs = false;
   };
 
