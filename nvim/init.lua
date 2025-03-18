a6b1329393fa0a470c2b96b18c460ab0c2c3ece7
bitbloxhub <45184892+bitbloxhub@users.noreply.github.com>
2025-03-18 19:57:50 +0000

    feat(nvim): reccomended codecompanion binds
    https://codecompanion.olimorris.dev/getting-started.html#suggested-plugin-workflow

--- nvim/init.lua
+++ nvim/init.lua
@@ -469,6 +469,18 @@ now(function()
 			end,
 		},
 	})
+
+	vim.keymap.set({ "n", "v" }, "<C-a>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
+	vim.keymap.set(
+		{ "n", "v" },
+		"<LocalLeader>a",
+		"<cmd>CodeCompanionChat Toggle<cr>",
+		{ noremap = true, silent = true }
+	)
+	vim.keymap.set("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
+
+	-- Expand 'cc' into 'CodeCompanion' in the command line
+	vim.cmd([[cab cc CodeCompanion]])
 end)
 -- Safely execute later
 later(function()
