83f1bb990fb56dcab51132fb262b2633417403b7
bitbloxhub <45184892+bitbloxhub@users.noreply.github.com>
2025-03-18 20:03:53 +0000

    feat(nvim): use fzf-lua for vim.ui.select

--- nvim/init.lua
+++ nvim/init.lua
@@ -199,6 +199,16 @@ now(function()
 end)
 now(function()
 	require("fzf-lua").setup({})
+	require("fzf-lua").register_ui_select(function(_, items)
+		local min_h, max_h = 0.15, 0.70
+		local h = (#items + 4) / vim.o.lines
+		if h < min_h then
+			h = min_h
+		elseif h > max_h then
+			h = max_h
+		end
+		return { winopts = { height = h, width = 0.60, row = 0.40 } }
+	end)
 end)
 now(function()
 	require("render-markdown").setup({})
