bba6ec7247d541888a7535a99815d374cfcc431c
bitbloxhub <45184892+bitbloxhub@users.noreply.github.com>
2025-03-24 16:42:35 +0000

    chore: fmt

--- home/cuda.nix
+++ home/cuda.nix
@@ -6,10 +6,17 @@
 }:
 
 {
-  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) (if (nvidia) then [
-    "cuda_cccl"
-    "cuda_cudart"
-    "libcublas"
-    "cuda_nvcc"
-  ] else []);
+  nixpkgs.config.allowUnfreePredicate =
+    pkg:
+    builtins.elem (lib.getName pkg) (
+      if (nvidia) then
+        [
+          "cuda_cccl"
+          "cuda_cudart"
+          "libcublas"
+          "cuda_nvcc"
+        ]
+      else
+        [ ]
+    );
 }
