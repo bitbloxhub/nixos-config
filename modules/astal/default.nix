014ee73f4ab7d0a087c5bed60ee766db550309c7
bitbloxhub <45184892+bitbloxhub@users.noreply.github.com>
2025-12-16 21:50:14 +0000

    refactor(astal): move `astal-shell` to a perSystem package

--- modules/astal/default.nix
+++ modules/astal/default.nix
@@ -32,53 +32,12 @@
       pkgs,
       ...
     }:
-    let
-      pnpmDeps = pkgs.pnpm_10.fetchDeps {
-        pname = "astal-shell-pnpm-deps";
-        src = ./.;
-
-        fetcherVersion = 2;
-        hash = "sha256-jQ9HOO8Cjh66C0ElatYmikTfMenjk5c5vANfZ4q7I2k=";
-
-        # From https://github.com/retrozinndev/colorshell/blob/babfd11/nix/colorshell.nix#L102
-        # fetcher version 2 fails if there are no *-exec files in the output
-        preFixup = ''
-          touch $out/.dummy-exec
-        '';
-      };
-    in
     {
       home.packages = lib.mkIf config.my.programs.astal.enable [
         (inputs.ags.packages.${pkgs.stdenv.hostPlatform.system}.ags.override {
           extraPackages = self.lib.agsExtraPackagesForPkgs pkgs;
         })
-        (pkgs.stdenv.mkDerivation {
-          inherit pnpmDeps;
-
-          name = "astal-shell";
-          src = ./.;
-
-          nativeBuildInputs = [
-            pkgs.nodejs_24
-            pkgs.pnpm_10.configHook
-            pkgs.wrapGAppsHook4
-            pkgs.gobject-introspection
-            inputs.ags.packages.${pkgs.stdenv.hostPlatform.system}.default
-          ];
-
-          buildInputs = [
-            pkgs.glib
-            pkgs.gjs
-          ]
-          ++ (self.lib.agsExtraPackagesForPkgs pkgs);
-
-          installPhase = ''
-            mv style.css style.old.css
-            ${pkgs.esbuild}/bin/esbuild --bundle style.old.css --outfile=style.css --supported:nesting=false
-            mkdir -p $out/bin
-            ags bundle app.ts $out/bin/astal-shell
-          '';
-        })
+        inputs.self.packages.${pkgs.stdenv.hostPlatform.system}.astal-shell
       ];
 
       home.file."${config.xdg.dataHome}/astal-shell/icons/" = {
@@ -110,6 +69,21 @@
       inputs',
       ...
     }:
+    let
+      pnpmDeps = pkgs.pnpm_10.fetchDeps {
+        pname = "astal-shell-pnpm-deps";
+        src = ./.;
+
+        fetcherVersion = 2;
+        hash = "sha256-jQ9HOO8Cjh66C0ElatYmikTfMenjk5c5vANfZ4q7I2k=";
+
+        # From https://github.com/retrozinndev/colorshell/blob/babfd11/nix/colorshell.nix#L102
+        # fetcher version 2 fails if there are no *-exec files in the output
+        preFixup = ''
+          touch $out/.dummy-exec
+        '';
+      };
+    in
     {
       make-shells.default = {
         name = "nixos-config";
@@ -119,5 +93,33 @@
           })
         ];
       };
+
+      packages.astal-shell = pkgs.stdenv.mkDerivation {
+          inherit pnpmDeps;
+
+          name = "astal-shell";
+          src = ./.;
+
+          nativeBuildInputs = [
+            pkgs.nodejs_24
+            pkgs.pnpm_10.configHook
+            pkgs.wrapGAppsHook4
+            pkgs.gobject-introspection
+            inputs.ags.packages.${pkgs.stdenv.hostPlatform.system}.default
+          ];
+
+          buildInputs = [
+            pkgs.glib
+            pkgs.gjs
+          ]
+          ++ (self.lib.agsExtraPackagesForPkgs pkgs);
+
+          installPhase = ''
+            mv style.css style.old.css
+            ${pkgs.esbuild}/bin/esbuild --bundle style.old.css --outfile=style.css --supported:nesting=false
+            mkdir -p $out/bin
+            ags bundle app.ts $out/bin/astal-shell
+          '';
+        };
     };
 }
