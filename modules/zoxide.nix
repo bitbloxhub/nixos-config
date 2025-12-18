7d157f1a0f04e59031c06add060004e16778ec3b
bitbloxhub <45184892+bitbloxhub@users.noreply.github.com>
2025-12-18 18:27:58 +0000

    feat: add zoxide

---
+++ modules/zoxide.nix
@@ -0,0 +1,22 @@
+{
+  self,
+  ...
+}:
+{
+  flake.modules.generic.default = {
+    options.my.programs.zoxide = {
+      enable = self.lib.mkDisableOption "Zoxide";
+      enableNushellIntegration = self.lib.mkDisableOption "Zoxide Nushell integration";
+    };
+  };
+
+  flake.modules.homeManager.default =
+    {
+      config,
+      ...
+    }:
+    {
+      programs.zoxide.enable = config.my.programs.zoxide.enable;
+      programs.zoxide.enableNushellIntegration = config.my.programs.atuin.enableNushellIntegration;
+    };
+}
