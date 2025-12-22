3d2188aac6e5e966bbe9199db1fa4b3ce29d8fe4
bitbloxhub <45184892+bitbloxhub@users.noreply.github.com>
2025-12-22 02:26:21 +0000

    feat: flake extra substituters

--- modules/nix.nix
+++ modules/nix.nix
@@ -2,7 +2,33 @@
   lib,
   ...
 }:
+let
+  extra-substituters = [
+    "https://cache.nixos.org"
+    "https://nix-community.cachix.org"
+    "https://cuda-maintainers.cachix.org"
+    "https://catppuccin.cachix.org"
+    "https://cache.lix.systems"
+    "https://niri.cachix.org"
+    "https://ags.cachix.org"
+    "https://yazi.cachix.org"
+  ];
+  extra-trusted-public-keys = [
+    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+    "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
+    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+    "catppuccin.cachix.org-1:noG/4HkbhJb+lUAdKrph6LaozJvAeEEZj4N732IysmU="
+    "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
+    "ags.cachix.org-1:naAvMrz0CuYqeyGNyLgE010iUiuf/qx6kYrUv3NwAJ8="
+    "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k="
+  ];
+in
 {
+  flake-file.nixConfig = {
+    inherit extra-substituters extra-trusted-public-keys;
+  };
+
   flake.modules.nixos.default =
     {
       pkgs,
@@ -17,26 +43,7 @@
             "nix-command"
             "flakes"
           ];
-          substituters = [
-            "https://cache.nixos.org"
-            "https://nix-community.cachix.org"
-            "https://cuda-maintainers.cachix.org"
-            "https://catppuccin.cachix.org"
-            "https://cache.lix.systems"
-            "https://niri.cachix.org"
-            "https://ags.cachix.org"
-            "https://yazi.cachix.org"
-          ];
-          trusted-public-keys = [
-            "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-            "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
-            "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-            "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
-            "catppuccin.cachix.org-1:noG/4HkbhJb+lUAdKrph6LaozJvAeEEZj4N732IysmU="
-            "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
-            "ags.cachix.org-1:naAvMrz0CuYqeyGNyLgE010iUiuf/qx6kYrUv3NwAJ8="
-            "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k="
-          ];
+          inherit extra-substituters extra-trusted-public-keys;
         };
       };
     };
