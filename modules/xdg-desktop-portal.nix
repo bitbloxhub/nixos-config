72c60ecd4aaf7a3a3b40abe67729442771354607
bitbloxhub <45184892+bitbloxhub@users.noreply.github.com>
2026-03-03 22:04:56 +0000

    fix(xdg-desktop-portal): add portals system-wide for some reason

--- modules/xdg-desktop-portal.nix
+++ modules/xdg-desktop-portal.nix
@@ -3,59 +3,75 @@
     { aspect, ... }:
     {
       includes = [ aspect._.xdg-desktop-portal ];
-      _.xdg-desktop-portal.homeManager =
-        {
-          config,
-          pkgs,
-          ...
-        }:
-        {
-          xdg.portal = {
-            enable = true;
-            extraPortals = [
+      _.xdg-desktop-portal = {
+        nixos =
+          {
+            pkgs,
+            ...
+          }:
+          {
+            # TODO: why do we need this?
+            xdg.portal.extraPortals = [
               pkgs.xdg-desktop-portal-gtk
               pkgs.xdg-desktop-portal
               pkgs.xdg-desktop-portal-gnome # Niri uses this for screensharing
               pkgs.xdg-desktop-portal-termfilechooser
             ];
-            config = {
-              common = {
-                default = "gtk";
-                "org.freedesktop.impl.portal.ScreenCast" = "gnome";
-                "org.freedesktop.impl.portal.Screenshot" = "gnome";
-                "org.freedesktop.impl.portal.FileChooser" = "termfilechooser";
+          };
+        homeManager =
+          {
+            config,
+            pkgs,
+            ...
+          }:
+          {
+            xdg.portal = {
+              enable = true;
+              extraPortals = [
+                pkgs.xdg-desktop-portal-gtk
+                pkgs.xdg-desktop-portal
+                pkgs.xdg-desktop-portal-gnome # Niri uses this for screensharing
+                pkgs.xdg-desktop-portal-termfilechooser
+              ];
+              config = {
+                common = {
+                  default = "gtk";
+                  "org.freedesktop.impl.portal.ScreenCast" = "gnome";
+                  "org.freedesktop.impl.portal.Screenshot" = "gnome";
+                  "org.freedesktop.impl.portal.FileChooser" = "termfilechooser";
+                };
               };
             };
+
+            xdg.configFile."xdg-desktop-portal-termfilechooser/config".text =
+              # TOML
+              ''
+                [filechooser]
+                cmd=${pkgs.xdg-desktop-portal-termfilechooser}/share/xdg-desktop-portal-termfilechooser/yazi-wrapper.sh
+                env=TERMFILECHOOSER=1
+                env=TERMCMD=wezterm start --always-new-process --class wezterm.termfilechooser --workspace termfilechooser
+                default_dir=$HOME
+                open_mode=default
+                save_mode=default
+              '';
+
+            programs.niri.settings.window-rules = [
+              {
+                matches = [
+                  { app-id = "^wezterm.termfilechooser$"; }
+                ];
+                open-floating = true;
+                default-column-width = {
+                  fixed = 1440;
+                };
+                default-window-height = {
+                  fixed = 720;
+                };
+              }
+            ];
+
+            systemd.user.packages = [ pkgs.xdg-desktop-portal ] ++ config.xdg.portal.extraPortals;
           };
-
-          xdg.configFile."xdg-desktop-portal-termfilechooser/config".text =
-            # TOML
-            ''
-              [filechooser]
-              cmd=${pkgs.xdg-desktop-portal-termfilechooser}/share/xdg-desktop-portal-termfilechooser/yazi-wrapper.sh
-              env=TERMFILECHOOSER=1
-              env=TERMCMD=wezterm start --always-new-process --class wezterm.termfilechooser --workspace termfilechooser
-              default_dir=$HOME
-              open_mode=default
-              save_mode=default
-            '';
-
-          programs.niri.settings.window-rules = [
-            {
-              matches = [
-                { app-id = "^wezterm.termfilechooser$"; }
-              ];
-              open-floating = true;
-              default-column-width = {
-                fixed = 1440;
-              };
-              default-window-height = {
-                fixed = 720;
-              };
-            }
-          ];
-
-          systemd.user.packages = [ pkgs.xdg-desktop-portal ] ++ config.xdg.portal.extraPortals;
-        };
+      };
     };
 }
