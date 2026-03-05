4394784f447e23c7c457fe302842601887672842
bitbloxhub <45184892+bitbloxhub@users.noreply.github.com>
2026-03-05 22:40:01 +0000

    feat: add btop

---
+++ modules/btop.nix
@@ -0,0 +1,28 @@
+{
+  flake.aspects = {
+    cli =
+      { aspect, ... }:
+      {
+        includes = [ aspect._.btop ];
+        _.btop.homeManager = {
+          programs.btop = {
+            enable = true;
+            settings = {
+              theme_background = false;
+              update_ms = 500;
+            };
+          };
+        };
+      };
+
+    nvidia.homeManager = {
+      nixpkgs.overlays = [
+        (_final: prev: {
+          btop = prev.btop.override {
+            cudaSupport = true;
+          };
+        })
+      ];
+    };
+  };
+}
