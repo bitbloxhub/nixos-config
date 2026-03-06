4187eda96b21926ca939442eaba0f6314e0a0a54
bitbloxhub <45184892+bitbloxhub@users.noreply.github.com>
2026-03-06 18:14:01 +0000

    feat(btop): add vim keybinds

--- modules/btop.nix
+++ modules/btop.nix
@@ -8,6 +8,7 @@
           programs.btop = {
             enable = true;
             settings = {
+              vim_keys = true;
               theme_background = false;
               update_ms = 500;
             };
