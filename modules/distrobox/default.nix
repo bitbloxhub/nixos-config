92456c8d74f977ab788602f18e082c759a2bedc3
bitbloxhub <45184892+bitbloxhub@users.noreply.github.com>
2026-03-16 19:39:04 +0000

    feat: distrobox-host-exec that works better with direnv
    Created by pi.dev with GLM-5 through OpenRouter

---
+++ modules/distrobox/default.nix
@@ -0,0 +1,27 @@
+{
+	flake.aspects.cli =
+		{ aspect, ... }:
+		{
+			includes = [ aspect._.distrobox ];
+			_.distrobox.homeManager =
+				{
+					pkgs,
+					lib,
+					...
+				}:
+				{
+					home.packages = [
+						pkgs.yay
+						(lib.hiPrio (pkgs.writeShellScriptBin "distrobox-host-exec" (builtins.readFile ./distrobox-host-exec)))
+					];
+					programs.distrobox = {
+						enable = true;
+						settings = {
+							container_manager = "podman";
+							container_image_default = "ghcr.io/archlinux/archlinux:base";
+							container_name_default = "arch";
+						};
+					};
+				};
+		};
+}
