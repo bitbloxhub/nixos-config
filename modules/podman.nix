731b79299b70b2367213e8447a7c68ca32713e99
bitbloxhub <45184892+bitbloxhub@users.noreply.github.com>
2026-03-23 17:16:10 +0000

    fix(podman): impermanence setup

--- modules/podman.nix
+++ modules/podman.nix
@@ -3,13 +3,25 @@
     { aspect, ... }:
     {
       includes = [ aspect._.podman ];
-      _.podman.nixos = {
-        virtualisation = {
-          containers.enable = true;
-          podman = {
-            enable = true;
-            dockerCompat = true;
+      _.podman = {
+        nixos = {
+          virtualisation = {
+            containers.enable = true;
+            podman = {
+              enable = true;
+              dockerCompat = true;
+            };
+            environment.persistence."/persistent" = {
+              directories = [
+                "/var/lib/containers/storage"
+              ];
+            };
           };
+        };
+        homeManager = {
+          home.persistence."/persistent".directories = [
+            ".local/share/containers/storage"
+          ];
         };
       };
     };
