a90cee34353d2bc5aec607106244e2f81815b5af
bitbloxhub <45184892+bitbloxhub@users.noreply.github.com>
2026-04-04 20:26:31 +0000

    feat(emacs): add more org-roam keybinds

--- modules/emacs/post-init.el
+++ modules/emacs/post-init.el
@@ -503,11 +503,22 @@
 (use-package
   org-roam
   :custom (org-roam-directory "~/notes") (org-roam-completion-everywhere t)
+  :bind
+  (("C-c n l" . org-roam-buffer-toggle)
+   ("C-c n f" . org-roam-node-find)
+   ("C-c n g" . org-roam-graph)
+   ("C-c n i" . org-roam-node-insert)
+   ("C-c n c" . org-roam-capture)
+   ;; Dailies
+   ("C-c n j" . org-roam-dailies-capture-today))
   :config
   ;; If you're using a vertical completion framework, you might want a more informative completion interface
   (setq org-roam-node-display-template
         (concat "${title:*} " (propertize "${tags:100}" 'face 'org-tag)))
-  (org-roam-db-autosync-mode))
+  (org-roam-db-autosync-mode)
+  (add-to-list
+   'display-buffer-alist '("\\*Org Select\\*" (display-buffer-below-selected)))
+  (add-to-list 'display-buffer-alist '("CAPTURE*" (display-buffer-same-window))))
 
 (use-package ox-json :ensure nil)
 
