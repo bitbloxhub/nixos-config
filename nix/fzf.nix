711a4765dbd61303128fe66f45590f41ddcf3cb5
bitbloxhub <45184892+bitbloxhub@users.noreply.github.com>
2026-07-10 20:23:03 +0000

    fix(fzf): no bg color

--- nix/fzf.nix
+++ nix/fzf.nix
@@ -1,4 +1,8 @@
 {
+  lib,
+  ...
+}:
+{
   flake.aspects.cli =
     { aspect, ... }:
     {
@@ -7,6 +11,7 @@
         programs.fzf = {
           enable = true;
           historyWidget.command = "";
+          colors.bg = lib.mkForce "";
         };
       };
     };
