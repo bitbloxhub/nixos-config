9c488c60e1a4421a64671db35fcd0c069bf026f7
bitbloxhub <45184892+bitbloxhub@users.noreply.github.com>
2026-08-19 16:48:11 +0000

    chore: run nix fmt

--- nix/prismlauncher.nix
+++ nix/prismlauncher.nix
@@ -13,33 +13,36 @@
         pkgs,
         ...
       }:
-      lib.mkIf user.config.prismlauncher.enable (let
-        bw = (inputs.nix-bwrapper.lib.mkNixBwrapper pkgs);
-      in {
-        home.packages = [
-          (bw.bwrapperEval {
-            imports = [
-              bw.bwrapperPresets.desktop
-            ];
-            app = {
-              package = pkgs.prismlauncher.override {
-                additionalLibs = [ pkgs.libvlc ];
-                additionalPrograms = [
-                  pkgs.ffmpeg
-                  pkgs.vlc
+      lib.mkIf user.config.prismlauncher.enable (
+        let
+          bw = inputs.nix-bwrapper.lib.mkNixBwrapper pkgs;
+        in
+        {
+          home.packages = [
+            (bw.bwrapperEval {
+              imports = [
+                bw.bwrapperPresets.desktop
+              ];
+              app = {
+                package = pkgs.prismlauncher.override {
+                  additionalLibs = [ pkgs.libvlc ];
+                  additionalPrograms = [
+                    pkgs.ffmpeg
+                    pkgs.vlc
+                  ];
+                };
+                addPkgs = [
+                  pkgs.kdePackages.qtstyleplugin-kvantum
+                  pkgs.fira-code
                 ];
               };
-              addPkgs = [
-                pkgs.kdePackages.qtstyleplugin-kvantum
-                pkgs.fira-code
-              ];
-            };
-            flatpak.manifestFile = pkgs.fetchurl {
-              url = "https://raw.githubusercontent.com/flathub/org.prismlauncher.PrismLauncher/067f68f/org.prismlauncher.PrismLauncher.yml";
-              hash = "sha256-1hGHnLClSKsGWu0/LvamvwbTciFqtvSsHgRMGxk834Q=";
-            };
-          }).config.build.package
-        ];
-      });
+              flatpak.manifestFile = pkgs.fetchurl {
+                hash = "sha256-1hGHnLClSKsGWu0/LvamvwbTciFqtvSsHgRMGxk834Q=";
+                url = "https://raw.githubusercontent.com/flathub/org.prismlauncher.PrismLauncher/067f68f/org.prismlauncher.PrismLauncher.yml";
+              };
+            }).config.build.package
+          ];
+        }
+      );
   };
 }
